aa0e665d79b36f65371fcaff7f3244e5eb189e47
Julian Zhu <yjyzhum@sunnyoptical.com>
2022-03-11 09:13:03 +0800

    add android clean script

---
+++ opengrok-clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+rm -rf cts compatibility 
+rm -r dalvik developers development docs 
+rm -rf external rm -rf libcore libnativehelper 
+rm -rf ndk pdk platform_testing 
+rm -rf prebuilts RKDocs rkst RKTools rockdev 
+rm -rf sdk toolchian tools
